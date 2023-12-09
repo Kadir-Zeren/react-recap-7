537cb8cc9ed47f2c7f326c15962a135cac7c5bf2
kadir-zeren <k4dzeren@gmail.com>
2023-12-09 12:15:08 +0300

    v4

--- src/components/1-forms/form.jsx
+++ src/components/1-forms/form.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 
 const Form = () => {
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleUsername = (e) => {
     setUsername(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`
+        username:${username}
+        email:${email}
+        password:${password}
+    `);
+    setEmail("");
+    setPassword("");
+    setUsername("");
+  };
+
   return (
     <div className="container mt-4">
       <h2 className="text-center text-success">FORMS IN REACT</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="username" className="form-label">
             Hello {username}
@@ -21,18 +35,21 @@ const Form = () => {
             id="username"
             aria-describedby="emailHelp"
             onChange={handleUsername}
+            value={username}
           />
         </div>
 
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
-            Email address
+            Email address <span className="text-danger">{email}</span>
           </label>
           <input
             type="email"
             className="form-control"
             id="email"
             aria-describedby="emailHelp"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </div>
 
@@ -40,7 +57,13 @@ const Form = () => {
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" id="password" />
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </div>
 
         <div className="text-center">
