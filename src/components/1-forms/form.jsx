6c74487b1ff4f8586361d0782a7fec17d2c3c89f
kadir-zeren <k4dzeren@gmail.com>
2023-12-09 11:52:58 +0300

    v2

--- src/components/1-forms/form.jsx
+++ src/components/1-forms/form.jsx
@@ -1,44 +1,44 @@
 const Form = () => {
   return (
     <div className="container mt-4">
+      <h2 className="text-center text-success">FORMS IN REACT</h2>
       <form>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
+          <label htmlFor="username" className="form-label">
+            Hello
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
             Email address
           </label>
           <input
             type="email"
             className="form-control"
-            id="exampleInputEmail1"
+            id="email"
             aria-describedby="emailHelp"
           />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
         </div>
+
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
+          <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
+          <input type="password" className="form-control" id="password" />
         </div>
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Check me out
-          </label>
+
+        <div className="text-center">
+          <button type="submit" className="btn btn-success">
+            Submit
+          </button>
         </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
       </form>
     </div>
   );
